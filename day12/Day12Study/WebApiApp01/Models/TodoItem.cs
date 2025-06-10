7a008c95f91047113eb9eb9ed0836063db896f30
BoKyung Ryu <dbqhrud5789@naver.com>
2025-06-10 16:15:48 +0900

    12일차 todolist 커밋

---
+++ day12/Day12Study/WebApiApp01/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mime;
+
+namespace WebApiApp01.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required] // NotNull. 일 경우는 string에 ?(Nullable)을 삭제할 것
+        [Column(TypeName = "VARCHAR(100)")]  // 이거 사용안하면 칼럼이 longtext 로 생성됨
+        public string Title { get; set; }
+        [Required]
+        [Column(TypeName = "CHAR(8))")]  // ex) 20250610
+        public string TodoDate {  get; set; }
+        // boolean
+        public bool IsComplete { get; set; }
+    }
+}
